5a5bf7f3ff4c43aae9fb5a7c35665150e962125b
Abrar Hamim <abrarhamim@Abrars-MacBook-Air.local>
2023-11-19 11:31:20 +0600

    Profile ADD

--- todolist/Views/ProfileView.swift
+++ todolist/Views/ProfileView.swift
@@ -13,12 +13,60 @@ struct ProfileView: View {
     var body: some View {
         NavigationView {
             VStack{
+                if let user = viewModel.user {
+                    profile(user: user)
+                }
+                else {
+                    Text("Loading Profile...")
+                }
                 
             }
             .navigationTitle("Profile")
-            
         }
+        .onAppear{
+            viewModel.fetchUser()
+        }
+    }
+    
+    @ViewBuilder
+    func profile(user: User) -> some View {
+        //image
+        Image(systemName: "person.circle")
+            .resizable()
+            .aspectRatio(contentMode: .fit)
+            .foregroundColor(Color.blue)
+            .frame(width: 125, height: 125)
+            .padding()
+        //info
+        VStack(alignment: .leading) {
+            HStack {
+                Text("Name: ")
+                    .bold()
+                Text(user.name)
+            }
+            .padding()
+            HStack {
+                Text("Email: ")
+                    .bold()
+                Text(user.email)
+            }
+            .padding()
+            HStack {
+                Text("Mumber Since: ")
+                    .bold()
+                Text("\(Date(timeIntervalSince1970: user.joined).formatted(date: .abbreviated, time: .shortened))")
+            }
+            .padding()
+        }
+        .padding()
         
+        //sign Out
+        Button ("Log Out") {
+            viewModel.logOut()
+        }
+        .tint(.red)
+        .padding()
+        Spacer()
     }
 }
 
--- todolist/ViewsModels/ToDoListItemViewViewModel.swift
+++ todolist/ViewsModels/ToDoListItemViewViewModel.swift
@@ -5,9 +5,27 @@
 //  Created by Abrar Hamim on 16/11/23.
 //
 
+import FirebaseAuth
+import FirebaseFirestore
 import Foundation
 
 //viewModel for sigle to do list view(each now in items list)
 class ToDoListItemViewViewModel: ObservableObject {
     init() {}
+    
+    func toggleIsDone(item: ToDoListItem){
+        var itemCopy = item
+        itemCopy.setDone(!item.isDone)
+        
+        guard let uid = Auth.auth().currentUser?.uid else {
+            return
+        }
+        
+        let db = Firestore.firestore()
+        db.collection("users")
+            .document(uid)
+            .collection("todos")
+            .document(itemCopy.id)
+            .setData(itemCopy.asDictionary())
+    }
 }
