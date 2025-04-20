d251d6f6ab1fb577ea4f86ebc73403d6eb17dc90
Abrar hamim <abrarhamim4044h@gmail.com>
2025-04-20 20:11:17 +0600

    ignore file add

--- todolist/Views/ForgetpaswordView.swift
+++ todolist/Views/ForgetpaswordView.swift
@@ -25,10 +25,6 @@ struct ForgetpaswordView: View {
                     .padding()
                     
                 }
-            
-            
-            
-            
         }
     }
 }
